8f5734de6931481d24bdceb3374aa027f82f6320
Salih Akbas <salihhakbass@gmail.com>
2025-03-03 02:24:51 +0300

    Added typeConverter annotation and room functions

--- app/src/main/java/com/salihakbas/cvmaker/data/source/local/MainDao.kt
+++ app/src/main/java/com/salihakbas/cvmaker/data/source/local/MainDao.kt
@@ -1,6 +1,26 @@
 package com.salihakbas.cvmaker.data.source.local
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.salihakbas.cvmaker.data.model.CV
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface MainDao
+interface MainDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCV(cv: CV)
+
+    @Query("SELECT * FROM CV")
+    fun getAllCVs(): Flow<List<CV>>
+
+    @Query("SELECT * FROM CV WHERE id = :cvId")
+    suspend fun getCVById(cvId: Int): CV
+
+    @Delete
+    suspend fun deleteCV(cv: CV)
+
+
+}
--- app/src/main/java/com/salihakbas/cvmaker/data/source/local/MainRoomDB.kt
+++ app/src/main/java/com/salihakbas/cvmaker/data/source/local/MainRoomDB.kt
@@ -2,9 +2,13 @@ package com.salihakbas.cvmaker.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.salihakbas.cvmaker.common.Converters
+import com.salihakbas.cvmaker.data.model.CV
 import com.salihakbas.cvmaker.data.model.MainEntityModel
 
-@Database(entities = [MainEntityModel::class], version = 1, exportSchema = false)
+@Database(entities = [CV::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class MainRoomDB : RoomDatabase() {
     abstract fun mainDao(): MainDao
 }
